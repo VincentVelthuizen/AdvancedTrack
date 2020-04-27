ec20a2fa7d06bba37a99a2776df907c3b181b416
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-04-27 17:56:09 +0200

    exercise 12.5.9.3

--- how_to_think/chapter_11/section_5_9/TurtleGTX.py
+++ how_to_think/chapter_11/section_5_9/TurtleGTX.py
@@ -1,4 +1,9 @@
+import random
 from turtle import Turtle, Screen
+
+
+def get_flat_tyre_distance():
+    return random.random(500, 1000)
 
 
 class TurtleGTX(Turtle):
@@ -6,13 +11,19 @@ class TurtleGTX(Turtle):
     def __init__(self):
         super().__init__()
         self.odometer = 0
+        self.flat_tyre = get_flat_tyre_distance()
 
     def forward(self, distance):
+        if self.odometer >= self.flat_tyre:
+            raise Exception()
         super().forward(distance)
         self.odometer += abs(distance)
 
     def get_odo(self):
         return self.odometer
+
+    def change_tyre(self):
+        self.flat_tyre += get_flat_tyre_distance()
 
 
 if __name__ == "__main__":
