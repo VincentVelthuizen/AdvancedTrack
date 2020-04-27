9fb74dc71d1f77889c4ceac66f8e5bf6c1fe7abd
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-04-27 11:46:33 +0200

    Test function that is used to be part of chapter 4 but now seems to be missing

---
+++ how_to_think/test/test.py
@@ -0,0 +1,15 @@
+def test_equal(actual, expected, places=5):
+    """
+    Does the actual value equal the expected value?
+    for floats, places indicates how many places, right of the decimal, must be correct
+    """
+    if isinstance(expected, float):
+        if abs(actual-expected) < 10 **(-places):
+            print("\tPass")
+            return True
+    else:
+        if actual == expected:
+            print("\tPass")
+            return True
+    print("\tTest Failed: expected {} but got {}".format(expected, actual))
+    return False
