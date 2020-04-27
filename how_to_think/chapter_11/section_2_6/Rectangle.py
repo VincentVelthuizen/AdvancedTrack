ac73681e17ea88bff103f39037485e51717509c8
Vincent Velthuizen <mail@vincentvelthuizen.com>
2020-04-27 11:23:03 +0200

    section 11.2.2

--- how_to_think/chapter_11/section_2_6/Rectangle.py
+++ how_to_think/chapter_11/section_2_6/Rectangle.py
@@ -13,6 +13,16 @@ class Rectangle:
     def __str__(self):
         return "{0}, {1}, {2}".format(self.corner, self.width, self.height)
 
+    def grow(self, delta_width, delta_height):
+        """ Grow (or shrink) this object by the deltas """
+        self.width += delta_width
+        self.height += delta_height
+
+    def move(self, delta_x, delta_y):
+        """ Move this object by the deltas """
+        self.corner.x += delta_x
+        self.corner.y += delta_y
+
 
 if __name__ == "__main__":
     # this only gets executed when the file is run directly, not when it is included in another
@@ -21,3 +31,9 @@ if __name__ == "__main__":
 
     print("box ", box)
     print("bomb ", bomb)
+
+    box.grow(50, -50)
+    print("box ", box)
+
+    box.move(100, 80)
+    print("box ", box)
